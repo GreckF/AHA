1c617c7df00d5d2435c8103ad4c1dc6c033a957d
GreckF <3246567677@qq.com>
2025-03-16 11:39:23 +0800

    evaluator

--- src/AST.hs
+++ src/AST.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 module AST where
+import Data.Map (Map, fromList)
 
 data Lit a
   = LitStr String 
@@ -7,17 +8,36 @@ data Lit a
   | LitFloat Double 
   | LitBool Bool
   | LitList [a]
-  deriving (Show, Eq)
+  deriving Eq
+
+instance Show a => Show (Lit a) where 
+  show = \case 
+    LitStr s -> s 
+    LitInt n -> show n 
+    LitFloat n -> show n
+    LitBool b -> show b 
+    LitList ls -> show ls
 
 type Var = String 
 type FuncName = String
 
-data Op = OpPlus | OpMin | OpMul | OpDiv | OpLE | OpGE | OpEQ
-        | OpOr   | OpAnd | OpNot | OpMod | OpCons
+data Op = OpPlus | OpMin | OpMul | OpDiv | OpLT | OpGT | OpEQ
+        | OpOr   | OpAnd | OpMod | OpCons | OpAppend 
   deriving (Show, Eq)
 
-data PrimFun = Nil | ToInt | ToFloat | Show | None | Some | Length | BreakToList | Join
-  deriving (Show, Eq)
+data PrimCall = Nil | ToInt | ToFloat | Show | None | Some | Length | BreakToList | Join | Floor | Not
+  deriving (Show, Eq, Ord)
+
+primNames :: [(PrimCall, FuncName)]
+primNames = [ (Nil, "nil"), (ToInt, "toInt"), (ToFloat, "toFloat"), (Show, "show")
+            , (None, "none"), (Some, "some"), (Length, "length"), (BreakToList, "sequence")
+            , (Join, "join"), (Floor, "floor"), (Not, "not") ]
+
+primToName :: Map PrimCall FuncName
+primToName = fromList primNames
+
+nameToPrim :: Map FuncName PrimCall
+nameToPrim = fromList $ map (\(a,b)->(b,a)) primNames
 
 data Constructor = CNone | CSome
   deriving (Show, Eq) 
@@ -25,7 +45,7 @@ data Constructor = CNone | CSome
 class Arity f where 
   arity :: f -> Int 
 
-instance Arity PrimFun where 
+instance Arity PrimCall where 
   arity = \case 
     Nil -> 0
     ToInt -> 1 
@@ -36,12 +56,14 @@ instance Arity PrimFun where
     Length -> 1 
     BreakToList -> 1 
     Join -> 1
-
+    Floor -> 1
+    Not -> 1
+    
 data Pattern 
   = PVar Var 
   | PCon Constructor [Pattern]
   | PList [Pattern]
-  | PListCons Pattern Pattern
+  | PListCons Pattern Pattern 
   deriving (Show, Eq)
 
 data Clause = Clause Pattern Term 
@@ -51,10 +73,11 @@ data Term
   = If Term Term Term 
   | Lit (Lit Term)
   | Lam Var Term 
+  | Var Var   
   | App Term Term 
-  | BinOp Op Term Term
+  | BiOp Op Term Term
   | FunCall FuncName
-  | PrimFun PrimFun
+  | PrimCall PrimCall
   | Let Var Term Term 
   | Match Term Clause 
   deriving (Show, Eq)
--- src/Eval.hs
+++ src/Eval.hs
@@ -1,130 +1,286 @@
 {-# LANGUAGE LambdaCase #-}
-module Eval where 
+{-# LANGUAGE PatternSynonyms #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+{-# OPTIONS_GHC -Wno-missing-pattern-synonym-signatures #-}
+module Eval where
 
-import AST 
-import qualified Data.Map as M 
+import AST
+import qualified Data.Map as M
 import Data.Functor
 import Text.Read (readMaybe)
 import Data.List (intercalate)
+import Control.Monad (forM, join)
 
-data Value 
+data Value
   = VLit (Lit Value)
   | VLam (Value -> IO Value)
   | VFunCall FuncDecl [Value]
-  | VPrimCall PrimFun [Value]
+  | VPrimCall PrimCall [Value]
   | VCon Constructor [Value]
 
-data FuncDecl = FuncDecl 
+instance Eq Value where
+  VLit a == VLit b = a == b
+  VCon con vs == VCon con' vs'
+    | con == con' = all (uncurry (==)) (zip vs vs')
+    | otherwise = False
+  _ == _ = False
+
+instance Show Value where 
+  show = \case 
+    VLit l -> show l 
+    VCon con args -> "(" ++ unwords (show con : map show args) ++")"
+    VLam f -> "function"
+    _ -> "Error on arity."
+
+data FuncDecl = FuncDecl
   { funcName :: FuncName
   , arguments :: [Var]
   , funcBody :: Term
-  } deriving Show 
+  } deriving Show
 
-instance Arity FuncDecl where 
+instance Arity FuncDecl where
   arity (FuncDecl _ args _) = length args
 
-data Env = Env 
-  { values :: M.Map Var Value 
+data Env = Env
+  { values :: M.Map Var Value
   , functions :: M.Map FuncName FuncDecl
   }
 
-newVar :: Var -> Value -> Env -> Env 
-newVar x val env = env { values = M.insert x val (values env) } 
+newVar :: Var -> Value -> Env -> Env
+newVar x val env = env { values = M.insert x val (values env) }
 
-data TorV = T Term | V Value 
+newVars :: [(Var, Value)] -> Env -> Env
+newVars vs env = foldr (uncurry newVar) env vs
 
-instance Show TorV where 
-  show (T t) = show t 
-  show (V v) = "TODO"
+data TorV = T Term | V Value | TwoValue Value Value | Unknown
+
+instance Show TorV where
+  show (T t) = show t
+  show (V v) = show v
+  show (TwoValue a b) = show a ++ " or " ++ show b
+  show Unknown = "unknown term"
 
 data Err
-  = TypeErr1 TorV String 
-  | TypeErr2 TorV String String 
-  | TooManyArg (Either PrimFun FuncDecl) Int Int
+  = TypeErr1 TorV String
+  | TypeErr2 TorV String String
+  | TooManyArg (Either PrimCall FuncDecl) Int Int
+  | UnknownVar Var
+  | UnknownFun FuncName
+  | Can'tMatch Pattern Value
 
-instance Show Err where 
+instance Show Err where
   show = \case
-    TypeErr1 t exp -> "Type mismatch: \n  " 
-                     ++ "Expecting: " ++ exp 
+    TypeErr1 t exp -> "Type mismatch: \n  "
+                     ++ "Expecting: " ++ exp
                  ++ "\nIn:  " ++ show t
-    TypeErr2 t exp act -> "Type mismatch: \n  " 
-                         ++ "Expecting: " ++ exp ++ "\n  " 
-                         ++ "Actually: " ++ act 
+    TypeErr2 t exp act -> "Type mismatch: \n  "
+                         ++ "Expecting: " ++ exp ++ "\n  "
+                         ++ "Actually: " ++ act
                      ++ "\nIn:  " ++ show t
-    TooManyArg f exp act -> "Too many arguments on function: " ++ show f 
+    TooManyArg f exp act -> "Too many arguments on function: " ++ show f
                       ++ "\n  Expecting: " ++ show exp
-                      ++ "\n  Actually: " ++ show act 
+                      ++ "\n  Actually: " ++ show act
+    UnknownVar x -> "(TwoValue a b) variable:  " ++ x
+    UnknownFun f -> "(TwoValue a b) function:  " ++ f
+    Can'tMatch p v -> "Can not match value: " ++ show v ++ " with pattern: " ++ show p
 
 
-throwErr :: Err -> IO a 
+throwErr :: Err -> IO a
 throwErr e = fail (show e)
 
-evalLit :: Env -> Lit Term -> IO (Lit Value) 
-evalLit env = \case 
+evalLit :: Env -> Lit Term -> IO (Lit Value)
+evalLit env = \case
   LitStr s   -> pure (LitStr s)
   LitInt n   -> pure (LitInt n)
   LitBool b  -> pure (LitBool b)
   LitFloat f -> pure (LitFloat f)
-  LitList ls -> LitList <$> mapM (eval env) ls 
+  LitList ls -> LitList <$> mapM (eval env) ls
 
 showValue :: Value -> IO String
-showValue = \case 
+showValue = \case
   VLit v -> showLit v
-  VCon con args -> showCon con args 
+  VCon con args -> showCon con args
   v -> throwErr $ TypeErr2 (V v) "any expect function" "function"
-  where 
-    showLit = \case 
-      LitStr s -> pure s 
-      LitInt n -> pure $ show n 
-      LitFloat n -> pure $ show n 
-      LitBool b -> pure $ show b 
-      LitList ls -> do 
+  where
+    showLit = \case
+      LitStr s -> pure s
+      LitInt n -> pure $ show n
+      LitFloat n -> pure $ show n
+      LitBool b -> pure $ show b
+      LitList ls -> do
         ls' <- mapM showValue ls
         pure $ show ls'
-    showCon con args = do 
-      args' <- mapM showValue args 
+    showCon con args = do
+      args' <- mapM showValue args
       pure $ "(" ++ unwords (show con : args') ++ ")"
-    
-evalPrimCall :: PrimFun -> [Value] -> IO Value 
-evalPrimCall prim args 
-  | length args < arity prim = pure $ VPrimCall prim args 
-  | length args > arity prim = throwErr $ TooManyArg (Left prim) (length args) (arity prim)
-  | otherwise = case prim of 
-      Nil -> pure $ VLit $ LitList [] 
-      ToInt -> case head args of 
-        VLit (LitStr s) -> case readMaybe s of 
-          Just n -> pure $ VCon CSome [VLit (LitInt n)]
-          Nothing -> pure $ VCon CNone [] 
-        v -> throwErr $ TypeErr1 (V v) "string"
-      ToFloat -> case head args of 
-        VLit (LitStr s) -> case readMaybe s of 
-          Just n -> pure $ VCon CSome [VLit (LitFloat n)]
-          Nothing -> pure $ VCon CNone [] 
-        v -> throwErr $ TypeErr1 (V v) "string"
-      Show -> do 
-        s <- showValue $ head args 
-        pure $ VLit $ LitStr s  
-      None -> pure $ VCon $ None 
-      _ -> _
 
-eval :: Env -> Term -> IO Value 
-eval env = \case 
+eval :: Env -> Term -> IO Value
+eval env = \case
   Lit lit -> (evalLit env lit <&> VLit)
   Lam x t -> pure . VLam $ \xv -> do eval (newVar x xv env) t
-  If b l r -> do 
-    bv <- eval env b 
-    case bv of 
-      VLit (LitBool bv') -> if bv' then eval env l else eval env r 
-      _ -> throwErr $ TypeErr1 b "bool"
-  App f x -> do 
-    fv <- eval env f 
-    case fv of 
-      VLam f' -> eval env x >>= f' 
-      VPrimCall f' args -> do 
-        xv <- eval env x 
-        evalPrimCall f' (args ++ [(x, xv)]) 
-      VFunCall f' args -> _ 
-      _ -> throwErr $ TypeErr1 f "function"
+  Var x -> case M.lookup x (values env) of
+    Just v -> pure v
+    Nothing -> throwErr $ UnknownVar x
+  If b l r -> do
+    bv <- eval env b
+    case bv of
+      VLit (LitBool bv') -> if bv' then eval env l else eval env r
+      _ -> throwErr $ TypeErr1 (T b) "bool"
+  App f x -> do
+    fv <- eval env f
+    case fv of
+      VLam f' -> eval env x >>= f'
+      VPrimCall f' args -> do
+        xv <- eval env x
+        evalPrimCall f' (args ++ [xv])
+      VFunCall f' args -> do
+        xv <- eval env x
+        evalFunCall (functions env) f' (args ++ [xv])
+      _ -> throwErr $ TypeErr1 (T f) "function"
+  Let x b t -> do
+    bv <- eval env b
+    eval (newVar x bv env) t
+  BiOp op l r -> do
+    lv <- eval env l
+    rv <- eval env r
+    evalBiOp op (lv, rv)
+  Match t clause -> do
+    undefined
+  FunCall fun -> case M.lookup fun (functions env) of
+    Just decl -> pure $ VFunCall decl []
+    Nothing -> throwErr $ UnknownFun fun
+  PrimCall fun -> pure $ VPrimCall fun []
 
-  _ -> undefined
+match1 :: Value -> Pattern -> IO [(Var, Value)]
+match1 v p = case p of
+  PVar x -> pure [(x, v)]
+  PCon con ps -> case v of
+    VCon con' vs
+      | con == con' -> matchN vs ps
+    _ -> throwErr $ Can'tMatch p v
+  PList ps -> case v of
+    V_List vs -> matchN vs ps
+    _ -> throwErr $ Can'tMatch p v
+  PListCons x xs -> case v of
+    V_List (xv : xsv) -> do
+      this <- match1 xv x
+      rest <- match1 (V_List xsv) xs
+      pure $ this ++ rest
+    _ -> throwErr $ Can'tMatch p v
 
+matchN :: [Value] -> [Pattern] -> IO [(Var, Value)]
+matchN vs ps = case (vs, ps) of
+  ([], []) -> pure []
+  (v:vs, p:ps) -> do
+    this <- match1 v p
+    rest <- matchN vs ps
+    pure $ this ++ rest
+  _ -> error "impossible"
+
+pattern V_Int x = (VLit (LitInt x))
+pattern V_Str x = (VLit (LitStr x))
+pattern V_Float x = (VLit (LitFloat x))
+pattern V_List x = (VLit (LitList x))
+pattern V_Bool x = (VLit (LitBool x))
+
+evalBiOp :: Op -> (Value, Value) -> IO Value
+evalBiOp = \case
+  OpPlus -> \case
+    (V_Int a, V_Int b) -> pure $ V_Int $ a + b
+    (V_Float a, V_Float b) -> pure $ V_Float $ a + b
+    (V_Int a, V_Float b) -> pure $ V_Float $ fromIntegral a + b
+    (V_Float a, V_Int b) -> pure $ V_Float $ a + fromIntegral b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int or float"
+  OpMin -> \case
+    (V_Int a, V_Int b) -> pure $ V_Int $ a - b
+    (V_Float a, V_Float b) -> pure $ V_Float $ a - b
+    (V_Int a, V_Float b) -> pure $ V_Float $ fromIntegral a - b
+    (V_Float a, V_Int b) -> pure $ V_Float $ a - fromIntegral b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int or float"
+  OpMul -> \case
+    (V_Int a, V_Int b) -> pure $ V_Int $ a * b
+    (V_Float a, V_Float b) -> pure $ V_Float $ a * b
+    (V_Int a, V_Float b) -> pure $ V_Float $ fromIntegral a * b
+    (V_Float a, V_Int b) -> pure $ V_Float $ a * fromIntegral b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int or float"
+  OpDiv -> \case
+    (V_Int a, V_Int b) -> pure $ V_Int $ a `div` b
+    (V_Float a, V_Float b) -> pure $ V_Float $ a / b
+    (V_Int a, V_Float b) -> pure $ V_Float $ fromIntegral a / b
+    (V_Float a, V_Int b) -> pure $ V_Float $ a / fromIntegral b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int or float"
+  OpLT -> \case
+    (V_Int a, V_Int b) -> pure $ V_Bool $ a < b
+    (V_Float a, V_Float b) -> pure $ V_Bool $ a < b
+    (V_Int a, V_Float b) -> pure $ V_Bool $ fromIntegral a < b
+    (V_Float a, V_Int b) -> pure $ V_Bool $ a < fromIntegral b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int or float"
+  OpGT -> \case
+    (V_Int a, V_Int b) -> pure $ V_Bool $ a > b
+    (V_Float a, V_Float b) -> pure $ V_Bool $ a > b
+    (V_Int a, V_Float b) -> pure $ V_Bool $ fromIntegral a > b
+    (V_Float a, V_Int b) -> pure $ V_Bool $ a > fromIntegral b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int or float"
+  OpEQ -> \(a,b) -> pure $ V_Bool $ a == b
+  OpOr -> \case
+    (V_Bool a, V_Bool b) -> pure $ V_Bool $ a || b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "bool"
+  OpAnd -> \case
+    (V_Bool a, V_Bool b) -> pure $ V_Bool $ a && b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "bool"
+  OpMod -> \case
+    (V_Int a, V_Int b) -> pure $ V_Int $ mod a b
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "int"
+  OpCons -> \case
+    (v, V_List vs) -> pure $ V_List $ v : vs
+    (a,b) -> throwErr $ TypeErr1 (V b) "list"
+  OpAppend -> \case
+    (V_List ls, V_List ls') -> pure $ V_List $ ls ++ ls'
+    (V_Str s, V_Str s') -> pure $ V_Str $ s ++ s'
+    (a,b) -> throwErr $ TypeErr1 (TwoValue a b) "list or string"
+
+evalFunCall :: M.Map FuncName FuncDecl -> FuncDecl -> [Value] -> IO Value
+evalFunCall env (FuncDecl _ argNames body) args = eval (newVars (zip argNames args) (Env M.empty env)) body
+
+evalPrimCall :: PrimCall -> [Value] -> IO Value
+evalPrimCall prim args
+  | length args < arity prim = pure $ VPrimCall prim args
+  | length args > arity prim = throwErr $ TooManyArg (Left prim) (length args) (arity prim)
+  | otherwise = case prim of
+      Nil -> pure $ VLit $ LitList []
+      ToInt -> case head args of
+        VLit (LitStr s) -> case readMaybe s of
+          Just n -> pure $ VCon CSome [VLit (LitInt n)]
+          Nothing -> pure $ VCon CNone []
+        v -> throwErr $ TypeErr1 (V v) "string"
+      ToFloat -> case head args of
+        VLit (LitStr s) -> case readMaybe s of
+          Just n -> pure $ VCon CSome [VLit (LitFloat n)]
+          Nothing -> pure $ VCon CNone []
+        v -> throwErr $ TypeErr1 (V v) "string"
+      Show -> do
+        s <- showValue $ head args
+        pure $ VLit $ LitStr s
+      None -> pure $ VCon CNone []
+      Some -> pure $ VCon CSome [head args]
+      BreakToList -> case head args of
+        VLit (LitStr s) -> pure $ VLit $ LitList $ map (VLit . LitStr . (: "")) s
+        v -> throwErr $ TypeErr1 (V v) "string"
+      Join -> case head args of
+        VLit (LitList ls) -> do
+          strs <- forM ls $ \case
+                    (VLit (LitStr s)) -> pure s
+                    v -> throwErr $ TypeErr1 (V v) "string"
+          pure $ VLit $ LitStr $ join strs
+        v -> throwErr $ TypeErr1 (V v) "list"
+      Length -> case head args of
+        VLit (LitList ls) -> pure $ VLit $ LitInt $ length ls
+        VLit (LitStr ls) -> pure $ VLit $ LitInt $ length ls
+        v -> throwErr $ TypeErr1 (V v) "string"
+      Floor -> case head args of
+        V_Float x -> pure $ V_Int $ floor x
+        v -> throwErr $ TypeErr1 (V v) "float"
+      Not -> case head args of
+        V_Bool x -> pure $ V_Bool $ not x
+        v -> throwErr $ TypeErr1 (V v) "bool"
